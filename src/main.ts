7f3293e3ba52bcf1f5975ca22685db93151c58c0
Kenta Kawabata <kenjuly67@gmail.com>
2026-01-18 10:17:49 +0900

    モードチェンジを

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,28 @@ import './style.css'
 // test();
 
 stopwatch();
+
+// ストップウォッチ、タイマー、アラームの切り替え
+const $panels = document.querySelectorAll<HTMLDivElement>('.panel');
+const $changeModeBtn = document.querySelectorAll<HTMLButtonElement>('.change-btn');
+const el = document.getElementById("stopwatch");
+if (el !== null && el !== undefined) {
+  el.classList.remove("panel");
+}
+
+
+$changeModeBtn.forEach(($btn) => {
+  $btn.addEventListener('click', () => {
+    const targetId = $btn.dataset.target;
+    if (!targetId) return;
+
+    $panels.forEach(($panel) => {
+      $panel.classList.add("panel");
+    });
+
+    const $target = document.getElementById(targetId);
+    if ($target) {
+      $target.classList.remove("panel");
+    }
+  });
+}); 
